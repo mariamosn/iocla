11fc35a4fd3861c3c0bb2e35ee3e3031bd75c39e
mariamosn <mariamosneag@gmail.com>
2020-11-24 15:21:43 +0200

    lab06

--- laborator/lab-06/0-mean/mean.asm
+++ laborator/lab-06/0-mean/mean.asm
@@ -19,9 +19,25 @@ main:
 
     ; TODO1 - compute the sum of the vector numbers - store it in eax
 
+sum:
+    xor ebx, ebx
+    mov bx, word [num_array + (ecx - 1) * 2]
+    add eax, ebx
+    loop sum
+
     PRINTF32 `Sum of numbers: %d\n\x0`, eax
 
     ; TODO2 - compute the quotient of the mean
+    mov ebx, eax
+    shr ebx, 16
+    mov dx, bx
+    xor ebx, ebx
+    mov bx, ax
+    xor eax, eax
+    mov ax, bx
+    xor ebx, ebx
+    mov ebx, ARRAY_SIZE
+    div bx
 
     PRINTF32 `Mean of numbers: %d\x0`, eax
     PRINTF32 `.\x0`
@@ -30,6 +46,13 @@ main:
 compute_decimal_place:
 
     ; TODO3 - compute the current decimal place - store it in ax
+    xor eax, eax
+    mov ax, dx
+    xor edx, edx
+    mov edx, 10
+    mul dl
+    xor edx, edx
+    div bx
 
     PRINTF32 `%d\x0`, eax
     dec ecx
